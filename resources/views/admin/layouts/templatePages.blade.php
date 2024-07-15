5d1b2723600a76a00083832e3ee979b2a1a00912
ricardo <ricardo@in8.com.br>
2024-07-15 17:26:05 -0300

    connect views with tailwind

--- resources/views/admin/layouts/templatePages.blade.php
+++ resources/views/admin/layouts/templatePages.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title') - Laravel 11</title>
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     <header>header</header>
