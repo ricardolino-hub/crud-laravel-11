3ede42feedaf8c6518ada6777a931e0d4ad3cdad
ricardo <ricardo@in8.com.br>
2024-07-17 14:53:38 -0300

    Chanhe request all for validated

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -22,7 +22,7 @@ class UserController extends Controller
 
     public function store(StoreUserRequest $request)
     {
-        User::create(request()->all());
+        User::create($request->validated());
         return redirect()
             ->route('users.index')
             ->with('success', 'Usuário criado com sucesso!');
