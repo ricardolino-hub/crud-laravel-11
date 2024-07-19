f8ff68a0a0e612e3153fd7df26b76b9cf2108a21
ricardo <ricardo@in8.com.br>
2024-07-19 18:00:38 -0300

    Add auth middleware in users route

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,15 @@ use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::delete('/user/{user}', [UserController::class, 'destroy'])->name('users.destroy');
-Route::get('/user/{user}', [UserController::class, 'show'])->name('users.show');
-Route::put('/user/{user}', [UserController::class, 'update'])->name('users.update');
-Route::get('/user/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
-Route::post('/user', [UserController::class, 'store'])->name('users.store');
-Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::middleware('auth')->group(function () {
+    Route::delete('/user/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+    Route::get('/user/{user}', [UserController::class, 'show'])->name('users.show');
+    Route::put('/user/{user}', [UserController::class, 'update'])->name('users.update');
+    Route::get('/user/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::post('/user', [UserController::class, 'store'])->name('users.store');
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');  
+});
 
 Route::get('/', function () {
     return view('welcome');
